09d53dd8b524ae591ac67a5d28b6709b561a4121
Phanuwat Atchariyathin <phanuwat.atc@axonstech.com>
2024-07-15 22:03:43 +0700

    fix padding when keyboard opened

--- app/src/main/java/com/denine/diaryapp/presentation/screens/home/HomeContent.kt
+++ app/src/main/java/com/denine/diaryapp/presentation/screens/home/HomeContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -36,6 +37,7 @@ fun HomeContent(
         LazyColumn (
             modifier = Modifier
                 .padding(horizontal = 24.dp)
+                .navigationBarsPadding()
         ) {
             diaryNotes.forEach{ (localDate,diaries) ->
                 stickyHeader(key = localDate){
